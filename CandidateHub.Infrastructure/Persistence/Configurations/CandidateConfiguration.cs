f5a41a3ebd219e362ba852dac1d870f760f7866e
Santosh Shrestha <santosh.chaukate@gmail.com>
2024-12-20 22:04:41 +0545

    #initial-project : configuration for candidate created and efcore added.

---
+++ CandidateHub.Infrastructure/Persistence/Configurations/CandidateConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using CandidateHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CandidateHub.Infrastructure.Persistence.Configurations
+{
+    internal class CandidateConfiguration : BaseConfiguration<Candidate, int>
+    {
+        public override void Configure(EntityTypeBuilder<Candidate> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.FirstName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(p => p.LastName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(p => p.Email)
+                .HasMaxLength(255)
+                .IsRequired();
+            builder.HasIndex(h => h.Email)
+                .IsUnique();
+
+            builder.Property(p => p.PhoneNumber)
+                .HasMaxLength(20)
+                .IsRequired(false);
+
+            builder.Property(p => p.CallTimeInterval)
+                .HasMaxLength(20)
+                .IsRequired(false);
+
+            builder.Property(p => p.LinkedInUrl)
+                .HasMaxLength(255)
+                .IsRequired(false);
+
+            builder.Property(p => p.GitHubUrl)
+                .HasMaxLength(255)
+                .IsRequired(false);
+
+            builder.Property(p => p.Comments)
+                .IsRequired();
+        }
+    }
+}
