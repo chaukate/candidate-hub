6e8eca4301aef127b5c19f36809f5e9091ca1b1e
sshrestha-devfinity <sshrestha@devfinity.io>
2024-12-20 14:42:38 +0545

    #initial-project : infrastructure projected added and swagger package used.

--- CandidateHub.Api/Program.cs
+++ CandidateHub.Api/Program.cs
@@ -1,10 +1,13 @@
+using CandidateHub.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment}.json", optional: true, reloadOnChange: true);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddInfrastructure(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 
