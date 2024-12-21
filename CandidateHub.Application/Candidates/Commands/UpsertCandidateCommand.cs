def3975c13bed851bb8dd64b94480779578e6c8c
Santosh Shrestha <santosh.chaukate@gmail.com>
2024-12-21 22:40:47 +0545

    #initial-project : upsert candidate validator added.

--- CandidateHub.Application/Candidates/Commands/UpsertCandidateCommand.cs
+++ CandidateHub.Application/Candidates/Commands/UpsertCandidateCommand.cs
@@ -1,5 +1,6 @@
 ﻿using CandidateHub.Application.Interfaces;
 using CandidateHub.Domain.Entities;
+using FluentValidation;
 using MediatR;
 using System.Text.Json.Serialization;
 
@@ -67,5 +68,85 @@ namespace CandidateHub.Application.Candidates.Commands
         [JsonIgnore]
         public string CurrentUserName { get; set; }
 
+    }
+
+    public class UpsertCondidateValidator : AbstractValidator<UpsertCandidateCommand>
+    {
+        public UpsertCondidateValidator()
+        {
+            RuleFor(r => r.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required.")
+                .MinimumLength(2)
+                .WithMessage("Minimum character limit is 2.")
+                .MaximumLength(255)
+                .WithMessage("Maximum character limit is 255.");
+
+            RuleFor(r => r.LastName)
+                .NotEmpty()
+                .WithMessage("Last name is required.")
+                .MinimumLength(2)
+                .WithMessage("Minimum character limit is 2.")
+                .MaximumLength(255)
+                .WithMessage("Maximum character limit is 255.");
+
+            RuleFor(r => r.Email)
+                .NotEmpty()
+                .WithMessage("Email is required.")
+                .EmailAddress()
+                .WithMessage("Invalid email address.")
+                .MaximumLength(255)
+                .WithMessage("Maximum character limit is 255.");
+
+            RuleFor(r => r.PhoneNumber)
+                .Matches(@"^\+?[1-9]\d{1,14}$")
+                .WithMessage("Invalid phone number format.")
+                .When(candidate => !string.IsNullOrEmpty(candidate.PhoneNumber));
+
+            RuleFor(r => r.CallTimeInterval)
+                .MaximumLength(50)
+                .WithMessage("Maximum character limit is 50.")
+                .Must(m =>
+                {
+                    var times = m.Split('-');
+                    return DateTime.TryParse(times[0], out var start) &&
+                           DateTime.TryParse(times[1], out var end) &&
+                           start < end;
+                })
+                .WithMessage("Invalid time format. End time must be greater than start time.")
+                .When(w => !string.IsNullOrEmpty(w.CallTimeInterval));
+
+            RuleFor(r => r.LinkedInUrl)
+                .MaximumLength(255)
+                .WithMessage("Maximum character limit is 255.")
+                .Must(m =>
+                {
+                    if (Uri.TryCreate(m, UriKind.Absolute, out var url))
+                        return url.OriginalString.Contains("linkedin.com/in/");
+
+                    return false;
+                })
+                .WithMessage("Invalid linkedin url.")
+                .When(w => !string.IsNullOrEmpty(w.LinkedInUrl));
+
+            RuleFor(r => r.GitHubUrl)
+                .MaximumLength(255)
+                .WithMessage("Maximum character limit is 255.")
+                .Must(m =>
+                {
+                    if (Uri.TryCreate(m, UriKind.Absolute, out var url))
+                        return url.OriginalString.Contains("github.com/");
+
+                    return false;
+                })
+                .WithMessage("Invalid github url.")
+                .When(w => !string.IsNullOrEmpty(w.GitHubUrl));
+
+            RuleFor(r => r.Comments)
+                .NotEmpty()
+                .WithMessage("Comments are required.")
+                .MinimumLength(25)
+                .WithMessage("Minimum character limit is 25.");
+        }
     }
 }
